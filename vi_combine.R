1786e3b57bf525dbeb55dc22513b7fe7759089e9
Danielle Ethier <dethier@birdscanada.org>
2025-03-04 10:04:37 -0500

    update directory

--- vi_combine.R
+++ vi_combine.R
@@ -1,6 +1,6 @@
 ##Combine data outputs into one table for Catherine's uploads
 
-Trends <- list.files(path = "C:/Users/dethier/Documents/ethier-scripts/RPI-Trends/Output/2023/",  # Identify all CSV files
+Trends <- list.files(path = "C:/Users/ethie/Documents/ethier-scripts/RPI-Trends/Output/2023/",  # Identify all CSV files
                      pattern = "*TrendsSlope.csv", full.names = TRUE) %>% 
   lapply(read_csv) %>%                              # Store all files in list
   bind_rows                                         # Combine data sets into one data set 
@@ -8,13 +8,13 @@ Trends <- list.files(path = "C:/Users/dethier/Documents/ethier-scripts/RPI-Trend
 #remove row with only NA
 n<-nrow(Trends)
 Trends<-Trends[2:n,]
-Trends<-Trends %>% drop_na(results_code)
+#Trends<-Trends %>% drop_na(results_code)
 
 write.csv(Trends, "Output/2023/AllTrendsRPI.csv", row.names = FALSE)
 
 
 
-Indices <- list.files(path = "C:/Users/dethier/Documents/ethier-scripts/RPI-Trends/Output/2023/",  # Identify all CSV files
+Indices <- list.files(path = "C:/Users/ethie/Documents/ethier-scripts/RPI-Trends/Output/2023/",  # Identify all CSV files
                       pattern = "*AnnualIndices.csv", full.names = TRUE) %>% 
   lapply(read_csv) %>%                              # Store all files in list
   bind_rows                                         # Combine data sets into one data set 
@@ -22,6 +22,6 @@ Indices <- list.files(path = "C:/Users/dethier/Documents/ethier-scripts/RPI-Tren
 
 m<-nrow(Indices)
 Indices<-Indices[2:m,]
-Indices<-Indices %>% drop_na(results_code)
+#Indices<-Indices %>% drop_na(results_code)
 
 write.csv(Indices, "Output/2023/AllIndicesRPI.csv", row.names = FALSE)
